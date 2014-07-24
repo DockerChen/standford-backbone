35ec9297a5674cae5e80bd265c1b9813d09a7a1f
Yang Wu <wuyangjack1991@g>
2014-07-23 18:43:22 -0700

    Tabs script.

---
+++ dev/tabs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gnome-terminal --working-directory /home/jackwu/Projects/standford-backbone/ \
+	--tab -t "Git" -e "bash" \
+	--tab -t "Htop" -e "htop" \
+	--tab -t "Eclipse" -e "/home/jackwu/eclipse/eclipse" \
+	--tab -t "Mininet" -e "bash" \
+	--tab -t "Sublime" -e "sublime-text"
